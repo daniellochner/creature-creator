905c2d8ce10df59d1db985db63256e3eef8c67af
Daniel Lochner <daniel@daniellochner.com>
2021-12-06 16:09:45 +0200

    Add hash utility (for GetHash and VerifyHash)

---
+++ Assets/Plugins/External/Snippets/HashUtility.cs
@@ -0,0 +1,40 @@
+// Credit: https://docs.microsoft.com/en-us/dotnet/api/system.security.cryptography.hashalgorithm.computehash?view=net-6.0
+
+using System;
+using System.Security.Cryptography;
+using System.Text;
+using UnityEngine;
+
+public static class HashUtility
+{
+    public static string GetHash(this HashAlgorithm hashAlgorithm, string input)
+    {
+        // Convert the input string to a byte array and compute the hash.
+        byte[] data = hashAlgorithm.ComputeHash(Encoding.UTF8.GetBytes(input));
+
+        // Create a new Stringbuilder to collect the bytes
+        // and create a string.
+        var sBuilder = new StringBuilder();
+
+        // Loop through each byte of the hashed data
+        // and format each one as a hexadecimal string.
+        for (int i = 0; i < data.Length; i++)
+        {
+            sBuilder.Append(data[i].ToString("x2"));
+        }
+
+        // Return the hexadecimal string.
+        return sBuilder.ToString();
+    }
+
+    public static bool VerifyHash(this HashAlgorithm hashAlgorithm, string input, string hash)
+    {
+        // Hash the input.
+        var hashOfInput = GetHash(hashAlgorithm, input);
+
+        // Create a StringComparer an compare the hashes.
+        StringComparer comparer = StringComparer.OrdinalIgnoreCase;
+
+        return comparer.Compare(hashOfInput, hash) == 0;
+    }
+}
