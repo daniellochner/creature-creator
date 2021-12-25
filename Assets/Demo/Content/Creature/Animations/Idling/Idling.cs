be677ed99aa1f7a947ecb1dda4e03d8ea8e7c315
Daniel Lochner <daniel@daniellochner.com>
2021-12-25 04:33:39 +0200

    Reorganize creature animation assets

--- Assets/Demo/Content/Creature/Animations/Idling/Idling.cs
+++ Assets/Demo/Content/Creature/Animations/Idling/Idling.cs
@@ -11,7 +11,6 @@ namespace DanielLochner.Assets.CreatureCreator
         #region Fields
         [SerializeField] private float breatheHeight = 0.02f;
         [SerializeField] private float breatheTime = 0.5f;
-        [SerializeField] private float turnThreshold = 0.9f;
         [SerializeField, ReadOnly] private float idlingTime;
         #endregion
 
@@ -26,36 +25,11 @@ namespace DanielLochner.Assets.CreatureCreator
             {
                 m_MonoBehaviour.CreatureConstructor.Root.localPosition = new Vector3(0, breatheHeight * Mathf.Cos(idlingTime / breatheTime) - breatheHeight, 0);
                 idlingTime += Time.deltaTime;
-
-                for (int i = 0; i < m_MonoBehaviour.Legs.Length / 2; ++i)
-                {
-                    LegAnimator lleg = m_MonoBehaviour.LLegs[i];
-                    LegAnimator rleg = m_MonoBehaviour.RLegs[i];
-
-                    if (m_MonoBehaviour.IsTurningRight && (i % 2) == 0)
-                    {
-                        HandleLeg(rleg);
-                        HandleLeg(lleg);
-                    }
-                    else
-                    {
-                        HandleLeg(lleg);
-                        HandleLeg(rleg);
-                    }
-                }
             }
         }
         public override void OnSLStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
         {
             m_MonoBehaviour.CreatureConstructor.Root.localPosition = Vector3.zero;
-        }
-
-        private void HandleLeg(LegAnimator leg)
-        {
-            if (Vector3.Dot(leg.Anchor.forward, leg.LegConstructor.Extremity.forward) < turnThreshold && !leg.FlippedLeg.IsMovingToTarget)
-            {
-                leg.MoveToTarget(m_MonoBehaviour.CreatureConstructor.Body.TransformPoint(leg.DefaultFootPosition), 0.25f, 0.2f);
-            }
         }
         #endregion
     }
---
+++ Assets/Demo/Content/Creature/Animations/Turning/Turning.cs
@@ -0,0 +1,43 @@
+// Creature Creator - https://github.com/daniellochner/Creature-Creator
+// Copyright (c) Daniel Lochner
+
+using UnityEngine;
+using UnityEngine.Animations;
+
+namespace DanielLochner.Assets.CreatureCreator
+{
+    public class Turning : SceneLinkedSMB<CreatureAnimator>
+    {
+        #region Fields
+        [SerializeField] private float turnThreshold = 0.9f;
+        [SerializeField] private int direction = 1;
+        #endregion
+
+        #region Methods
+        public override void OnSLStateNoTransitionUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex, AnimatorControllerPlayable controller)
+        {
+            if (m_MonoBehaviour.Legs.Length > 0)
+            {
+                
+            }
+        }
+        public override void OnSLStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+        {
+            m_MonoBehaviour.CreatureConstructor.Root.localPosition = Vector3.zero;
+        }
+
+        protected void HandleLeg(LegAnimator leg)
+        {
+            //if (Vector3.Dot(leg.Anchor.forward, leg.LegConstructor.Extremity.forward) < turnThreshold && !leg.FlippedLeg.IsMovingToTarget)
+            //{
+            //    Vector3 position = m_MonoBehaviour.CreatureConstructor.Body.TransformPoint(leg.DefaultFootPosition);
+            //    Quaternion rotation = m_MonoBehaviour.CreatureConstructor.Body.rotation;
+            //    float timeToMove = 0.25f;
+            //    float liftHeight = 0.25f;
+
+            //    leg.MoveToTarget(position, rotation, timeToMove, liftHeight);
+            //}
+        }
+        #endregion
+    }
+}
