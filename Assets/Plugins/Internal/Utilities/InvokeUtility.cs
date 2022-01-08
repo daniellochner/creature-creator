f4e958cec6099aa45b4634bbb549881543644bfe
Daniel Lochner <daniel@daniellochner.com>
2022-01-09 01:38:34 +0200

    Add InvokeOverTime to InvokeUtility

--- Assets/Plugins/Internal/Utilities/InvokeUtility.cs
+++ Assets/Plugins/Internal/Utilities/InvokeUtility.cs
@@ -1,29 +1,46 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using UnityEngine;
-using UnityEngine.Events;
 
 namespace DanielLochner.Assets
 {
     public static class InvokeUtility
     {
-        public static Coroutine Invoke(this MonoBehaviour monoBehaviour, UnityAction invokeFunction, float delay = 0)
+        public static Coroutine Invoke(this MonoBehaviour monoBehaviour, Action invokeFunction, float delay = 0f)
         {
             return monoBehaviour.StartCoroutine(InvokeRoutine(invokeFunction, delay));
         }
-        public static Coroutine InvokeAtEndOfFrame(this MonoBehaviour monoBehaviour, UnityAction invokeFunction, float delay = 0)
+        public static Coroutine InvokeAtEndOfFrame(this MonoBehaviour monoBehaviour, Action invokeFunction)
         {
             return monoBehaviour.StartCoroutine(InvokeAtEndOfFrameRoutine(invokeFunction));
         }
+        public static Coroutine InvokeOverTime(this MonoBehaviour monoBehaviour, Action<float> invokeFunction, float time, float timeScale = 1f)
+        {
+            return monoBehaviour.StartCoroutine(InvokeOverTimeRoutine(invokeFunction, time, timeScale));
+        }
 
-        public static IEnumerator InvokeRoutine(UnityAction invokeFunction, float delay = 0)
+        public static IEnumerator InvokeRoutine(Action invokeFunction, float delay = 0f)
         {
             yield return new WaitForSeconds(delay);
             invokeFunction();
         }
-        public static IEnumerator InvokeAtEndOfFrameRoutine(UnityAction invokeFunction)
+        public static IEnumerator InvokeAtEndOfFrameRoutine(Action invokeFunction)
         {
             yield return new WaitForEndOfFrame();
             invokeFunction();
+        }
+        public static IEnumerator InvokeOverTimeRoutine(Action<float> onProgress, float timeToMove, float timeScale = 1f)
+        {
+            float timeElapsed = 0f, progress = 0f;
+            while (progress < 1f)
+            {
+                timeElapsed += Time.deltaTime * timeScale;
+                progress = timeElapsed / timeToMove;
+
+                onProgress(progress);
+                
+                yield return null;
+            }
         }
     }
 }
