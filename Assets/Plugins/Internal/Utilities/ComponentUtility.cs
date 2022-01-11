f0af89f22980c95ac455e55403c32349e148b737
Daniel Lochner <daniel@daniellochner.com>
2022-01-11 02:25:58 +0200

    Add ComponentUtility.cs

---
+++ Assets/Plugins/Internal/Utilities/ComponentUtility.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using UnityEngine;
+
+namespace DanielLochner.Assets
+{
+    public static class ComponentUtility
+    {
+        private static Dictionary<string, object> copy = new Dictionary<string, object>();
+        private static readonly BindingFlags flags = BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance;
+        
+        public static void CopyValues(this Component component, bool debug = false)
+        {
+            copy.Clear();
+            Type type = component.GetType();
+            while (type != typeof(Component))
+            {
+                FieldInfo[] fields = type.GetFields(flags);
+                foreach (FieldInfo field in fields)
+                {
+                    if (copy.ContainsKey(field.Name)) continue;
+                    copy.Add(field.Name, field.GetValue(component));
+                }
+                type = type.BaseType;
+            }
+            if (debug) Debug.Log($"Copied {copy.Count} fields from '{component.name}'.");
+        }
+        public static void PasteValues(this Component component, bool debug = false)
+        {
+            int count = 0;
+            Type type = component.GetType();
+            foreach (string name in copy.Keys)
+            {
+                while (type != typeof(Component))
+                {
+                    FieldInfo field = type.GetField(name, flags);
+                    if (field != null)
+                    {
+                        field.SetValue(component, copy[name]);
+                        count++;
+                        break;
+                    }
+                    type = type.BaseType;
+                }
+            }
+            if (debug) Debug.Log($"Pasted {count} values to '{component.name}'.");
+        }
+
+        public static void MoveToTop(this Component component)
+        {
+            while (UnityEditorInternal.ComponentUtility.MoveComponentUp(component)) ;
+        }
+    }
+}
