e8f833b4fa964d181110fdc2bfaa545c2e3021e2
Daniel Lochner <daniel@daniellochner.com>
2022-01-11 02:26:17 +0200

    Add DictionaryUtility.cs

---
+++ Assets/Plugins/Internal/Utilities/DictionaryUtility.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace DanielLochner.Assets
+{
+    public static class DictionaryUtility
+    {
+        public static K GetKeyFromValue<K, V>(this Dictionary<K, V> dict, V value)
+        {
+            return dict.FirstOrDefault(x => x.Value.Equals(value)).Key;
+        }
+    }
+}
