53fc4ca4da0837e8944dd86099fc22b4959b8bc9
Daniel Lochner <daniel@daniellochner.com>
2022-04-28 18:22:37 +0200

    Add skeleton prefabs for animals and their respective AI

---
+++ Assets/Demo/Game/Farm/Scripts/Runtime/Behaviours/AI/DogAI.cs
@@ -0,0 +1,14 @@
+// Creature Creator - https://github.com/daniellochner/Creature-Creator
+// Copyright (c) Daniel Lochner
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DanielLochner.Assets.CreatureCreator
+{
+    public class DogAI : AnimalAI
+    {
+
+    }
+}
