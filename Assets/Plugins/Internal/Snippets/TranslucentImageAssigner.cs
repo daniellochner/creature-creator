61e09300c6f415261a5c9dea2bc543b2347f3c4c
Daniel Lochner <daniel@daniellochner.com>
2022-05-11 11:40:17 +0200

    Fix TranslucentImage source assignment issue

---
+++ Assets/Plugins/Internal/Snippets/TranslucentImageAssigner.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using LeTai.Asset.TranslucentImage;
+
+namespace DanielLochner.Assets
+{
+    [RequireComponent(typeof(TranslucentImageSource))]
+    public class TranslucentImageAssigner : MonoBehaviour
+    {
+        private void Awake()
+        {
+            TranslucentImageSource source = GetComponent<TranslucentImageSource>();
+            foreach (TranslucentImage image in FindObjectsOfType<TranslucentImage>())
+            {
+                if (image.source == null)
+                {
+                    image.source = source;
+                }
+            }
+        }
+    }
+}
