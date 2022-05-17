ff8dc0d6df38a3074d197a85577629b06e9ccbb9
Daniel Lochner <daniel@daniellochner.com>
2022-05-17 02:43:31 +0200

    Separate creature energy and energy depleter components

--- Assets/Creature Creator/Scripts/Runtime/Behaviours/Creature/CreatureEnergy.cs
+++ Assets/Creature Creator/Scripts/Runtime/Behaviours/Creature/CreatureEnergy.cs
@@ -2,27 +2,18 @@
 // Copyright (c) Daniel Lochner
 
 using System;
-using System.Collections;
 using UnityEngine;
 
 namespace DanielLochner.Assets.CreatureCreator
 {
-    [RequireComponent(typeof(CreatureHealth))]
     public class CreatureEnergy : MonoBehaviour
     {
         #region Fields
-        [SerializeField] private float energyDepletionRate = 1f / 1200f;
-        [SerializeField] private float healthTickRate = 1f;
-        [SerializeField] private float healthTickDamage = 5f;
-        [Space]
         [SerializeField, ReadOnly] private float energy = 1f;
         #endregion
 
         #region Properties
-        private CreatureHealth CreatureHealth { get; set; }
-
         public Action<float> OnEnergyChanged { get; set; }
-        public bool DepleteEnergy { get; set; } = true;
 
         public float Energy
         {
@@ -31,33 +22,6 @@ namespace DanielLochner.Assets.CreatureCreator
             {
                 energy = Mathf.Clamp01(value);
                 OnEnergyChanged?.Invoke(energy);
-            }
-        }
-        #endregion
-
-        #region Methods
-        private void Awake()
-        {
-            CreatureHealth = GetComponent<CreatureHealth>();
-        }
-        private void Start()
-        {
-            StartCoroutine(EnergyDepletionRoutine(energyDepletionRate, healthTickRate, healthTickDamage));
-        }
-
-        private IEnumerator EnergyDepletionRoutine(float energyDepletionRate, float healthTickRate, float healthTickDamage)
-        {
-            while (!CreatureHealth.IsDead && DepleteEnergy)
-            {
-                yield return new WaitForSeconds(1f);
-                Energy -= energyDepletionRate;
-
-                if (Energy <= 0)
-                {
-                    CreatureHealth.TakeDamage(healthTickDamage);
-                    yield return new WaitForSeconds(1f / healthTickRate);
-                }
-                else yield return null;
             }
         }
         #endregion
---
+++ Assets/Creature Creator/Scripts/Runtime/Behaviours/Creature/CreatureEnergyDepleter.cs
@@ -0,0 +1,56 @@
+// Creature Creator - https://github.com/daniellochner/Creature-Creator
+// Copyright (c) Daniel Lochner
+
+using System;
+using System.Collections;
+using UnityEngine;
+
+namespace DanielLochner.Assets.CreatureCreator
+{
+	[RequireComponent(typeof(CreatureHealth))]
+	[RequireComponent(typeof(CreatureEnergy))]
+    public class CreatureEnergyDepleter : MonoBehaviour
+    {
+        #region Fields
+        [SerializeField] private float energyDepletionRate = 1f / 1200f;
+        [SerializeField] private float healthTickRate = 1f;
+        [SerializeField] private float healthTickDamage = 5f;
+        #endregion
+
+        #region Properties
+        public CreatureHealth Health { get; private set; }
+        public CreatureEnergy Energy { get; private set; }
+
+        public Action<float> OnEnergyChanged { get; set; }
+        public bool DepleteEnergy { get; set; } = true;
+        #endregion
+
+        #region Methods
+        private void Awake()
+        {
+            Health = GetComponent<CreatureHealth>();
+            Energy = GetComponent<CreatureEnergy>();
+		}
+        private void Start()
+        {
+            StartCoroutine(EnergyDepletionRoutine(energyDepletionRate, healthTickRate, healthTickDamage));
+        }
+
+        private IEnumerator EnergyDepletionRoutine(float energyDepletionRate, float healthTickRate, float healthTickDamage)
+        {
+            while (!Health.IsDead && DepleteEnergy)
+            {
+                yield return new WaitForSeconds(1f);
+                Energy.Energy -= energyDepletionRate;
+
+                if (Energy.Energy <= 0)
+                {
+                    Health.TakeDamage(healthTickDamage);
+                    yield return new WaitForSeconds(1f / healthTickRate);
+                }
+                else yield return null;
+            }
+        }
+        #endregion
+    }
+}
