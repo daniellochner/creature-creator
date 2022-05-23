e27a3a055a81915031d7b439b6cb4b1c9504a17f
Daniel Lochner <daniel@daniellochner.com>
2022-05-23 15:17:05 +0200

    Add creature dimensions

---
+++ Assets/Creature Creator/Scripts/Runtime/Data/Creature/CreatureDimensions.cs
@@ -0,0 +1,15 @@
+// Creature Creator - https://github.com/daniellochner/Creature-Creator
+// Copyright (c) Daniel Lochner
+
+using System;
+
+namespace DanielLochner.Assets.CreatureCreator
+{
+    [Serializable]
+    public class CreatureDimensions
+    {
+        public float length;
+        public float radius;
+        public float height;
+    }
+}
