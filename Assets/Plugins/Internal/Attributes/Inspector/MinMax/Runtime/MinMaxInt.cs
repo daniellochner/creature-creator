c07e27664da166ba4406f29f66f8b90068b42410
Daniel Lochner <daniel@daniellochner.com>
2022-05-25 22:54:16 +0200

    Add MinMaxInt

---
+++ Assets/Plugins/Internal/Attributes/Inspector/MinMax/Runtime/MinMaxInt.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace DanielLochner.Assets
+{
+    [Serializable]
+    public struct MinMaxInt
+    {
+        public int min;
+        public int max;
+
+        public MinMaxInt(int min, int max)
+        {
+            this.min = min;
+            this.max = max;
+        }
+
+        public int Random
+        {
+            get => UnityEngine.Random.Range(min, max);
+        }
+    }
+}
