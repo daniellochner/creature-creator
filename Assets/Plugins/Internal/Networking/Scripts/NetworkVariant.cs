0fdde5b22b641a3bde33c8f729c516b883bb84ba
Daniel Lochner <daniel@daniellochner.com>
2022-07-15 00:57:45 +0200

    Add network variants

--- Assets/Plugins/Internal/Networking/Scripts/NetworkVariant.cs
+++ Assets/Plugins/Internal/Networking/Scripts/NetworkVariant.cs
@@ -1,26 +1,10 @@
 using UnityEngine;
 
-namespace Unity.Netcode
+namespace DanielLochner.Assets
 {
     public class NetworkVariant : MonoBehaviour
     {
-        [SerializeField] private GameObject local;
-        [SerializeField] private GameObject remote;
-
-        public void Swap()
-        {
-            Transform localP = local.transform.parent;
-            int localSI = local.transform.GetSiblingIndex();
-
-            Transform remoteP = remote.transform.parent;
-            int remoteSI = remote.transform.GetSiblingIndex();
-
-
-            local.transform.parent = remoteP;
-            local.transform.SetSiblingIndex(remoteSI);
-
-            remote.transform.parent = localP;
-            remote.transform.SetSiblingIndex(localSI);
-        }
+        public GameObject Local;
+        public GameObject Remote;
     }
 }
