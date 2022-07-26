80cf1a895f2f66e6e30e47e5fdd64f5c165c13de
Daniel Lochner <daniel@daniellochner.com>
2022-07-26 22:46:50 +0200

    Add 10 minute delay to Island raft

--- Assets/Demo/Game/Island/Scripts/OceanRaft.cs
+++ Assets/Demo/Game/Island/Scripts/OceanRaft.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using Unity.Netcode;
 using UnityEngine;
 
@@ -5,19 +6,30 @@ namespace DanielLochner.Assets.CreatureCreator
 {
     public class OceanRaft : NetworkBehaviour
     {
+        [SerializeField] private int waitTime;
         [SerializeField] private Vector3 direction;
         [SerializeField] private float speed;
+        private bool move;
+        
+        private IEnumerator Start()
+        {
+            yield return new WaitForSeconds(waitTime);
+            move = true;
+        }
 
         private void FixedUpdate()
         {
-            transform.position += direction * speed * Time.fixedDeltaTime;
+            if (move)
+            {
+                transform.position += direction * speed * Time.fixedDeltaTime;
+            }
         }
 
         private void OnCollisionEnter(Collision collision)
         {
             if (collision.gameObject.layer == LayerMask.NameToLayer("Ground"))
             {
-                enabled = false;
+                move = false;
             }
         }
     }
