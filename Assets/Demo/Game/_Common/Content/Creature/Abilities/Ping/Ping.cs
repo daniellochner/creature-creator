2170fcfcadc17e0afd554c6f63fa605cb6b1b120
Daniel Lochner <daniel@daniellochner.com>
2022-07-31 00:46:52 +0200

    Add abilities to body parts

---
+++ Assets/Demo/Game/_Common/Content/Creature/Abilities/Ping/Ping.cs
@@ -0,0 +1,12 @@
+﻿// Creature Creator - https://github.com/daniellochner/Creature-Creator
+// Copyright (c) Daniel Lochner
+
+using UnityEngine;
+
+namespace DanielLochner.Assets.CreatureCreator.Abilities
+{
+    [CreateAssetMenu(menuName = "Creature Creator/Ability/Ping")]
+    public class Ping : Ability
+    {
+    }
+}
