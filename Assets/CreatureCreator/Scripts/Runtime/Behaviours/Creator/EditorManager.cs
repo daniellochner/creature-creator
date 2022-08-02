1ade34b25907e5837222368776114f5e3ba69b96
Daniel Lochner <daniel@daniellochner.com>
2022-08-02 02:13:30 +0200

    Move creatures to root folder instead of version folder

--- Assets/CreatureCreator/Scripts/Runtime/Behaviours/Creator/EditorManager.cs
+++ Assets/CreatureCreator/Scripts/Runtime/Behaviours/Creator/EditorManager.cs
@@ -136,6 +136,20 @@ namespace DanielLochner.Assets.CreatureCreator
         #endregion
 
         #region Methods
+        protected override void Awake()
+        {
+            base.Awake();
+
+            // Quick fix to move creatures from version directory to root.
+            // TODO: Remove in next version!
+
+            string oldDir = Path.Combine(Application.persistentDataPath, "1.0.0-b.1", "Creatures");
+            string newDir = Path.Combine(Application.persistentDataPath, "creature");
+            if (Directory.Exists(oldDir))
+            {
+                Directory.Move(oldDir, newDir);
+            }
+        }
         private void Update()
         {
             HandleKeyboardShortcuts();
@@ -250,7 +264,7 @@ namespace DanielLochner.Assets.CreatureCreator
             });
 
             // Options
-            creaturesDirectory = Path.Combine(Application.persistentDataPath, Application.version, "Creatures");
+            creaturesDirectory = Path.Combine(Application.persistentDataPath, "creature");
             if (!Directory.Exists(creaturesDirectory))
             {
                 Directory.CreateDirectory(creaturesDirectory);
