9964bc18ed2b0c76321382b5962255d8ed19e1ad
Daniel Lochner <daniel@daniellochner.com>
2022-08-20 05:44:15 +0200

    Add bite ability

--- Assets/Game/Worlds/_Common/Content/Creature/Abilities/Bite/Bite.cs
+++ Assets/Game/Worlds/_Common/Content/Creature/Abilities/Bite/Bite.cs
@@ -9,10 +9,35 @@ namespace DanielLochner.Assets.CreatureCreator.Abilities
     public class Bite : Ability
     {
         [SerializeField] private PlayerEffects.Sound[] biteSounds;
+        [SerializeField] private MinMax biteDamage;
+        [SerializeField] private float biteRadius;
+        private bool hasDealtDamage;
 
         public override void OnPerform()
         {
+            hasDealtDamage = false;
+
+            Player.Instance.Animator.Animator.GetBehaviour<Animations.Bite>().OnBite += OnBite;
+            Player.Instance.Animator.Params.SetTrigger("Body_Strike");
+        }
+        private void OnBite(MouthAnimator mouth)
+        {
             CreatureAbilities.GetComponent<PlayerEffects>().PlaySound(biteSounds);
+            if (!hasDealtDamage)
+            {
+                Collider[] colliders = Physics.OverlapSphere(mouth.transform.position, biteRadius);
+                foreach (Collider collider in colliders)
+                {
+                    CreatureBase creature = collider.GetComponent<CreatureBase>();
+                    if (creature != null && creature != Player.Instance)
+                    {
+                        creature.Health.TakeDamage(biteDamage.Random);
+                        hasDealtDamage = true;
+                    }
+                }
+            }
+
+            Player.Instance.Animator.Animator.GetBehaviour<Animations.Bite>().OnBite -= OnBite;
         }
     }
 }
---
+++ Assets/Game/Worlds/_Common/Content/Creature/Abilities/Growl/Roar.cs
@@ -0,0 +1,18 @@
+﻿// Creature Creator - https://github.com/daniellochner/Creature-Creator
+// Copyright (c) Daniel Lochner
+
+using UnityEngine;
+
+namespace DanielLochner.Assets.CreatureCreator.Abilities
+{
+    [CreateAssetMenu(menuName = "Creature Creator/Ability/Roar")]
+    public class Roar : Ability
+    {
+        [SerializeField] private PlayerEffects.Sound[] roarSounds;
+
+        public override void OnPerform()
+        {
+            CreatureAbilities.GetComponent<PlayerEffects>().PlaySound(roarSounds);
+        }
+    }
+}
