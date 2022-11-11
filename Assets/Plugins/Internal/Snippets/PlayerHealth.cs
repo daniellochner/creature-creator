61e06fd2edb0b41985c7b273bbeecbda3030a413
Daniel Lochner <daniel@daniellochner.com>
2022-11-11 13:23:05 +0200

    Add take damage force

--- Assets/Plugins/Internal/Snippets/PlayerHealth.cs
+++ Assets/Plugins/Internal/Snippets/PlayerHealth.cs
@@ -35,7 +35,7 @@ namespace DanielLochner.Assets
         public virtual float MaxHealth => 100f;
 
         public Action<float> OnHealthChanged { get; set; }
-        public Action<float> OnTakeDamage { get; set; }
+        public Action<float, Vector3> OnTakeDamage { get; set; }
         public Action OnDie { get; set; }
 
         public bool IsDead
@@ -48,7 +48,7 @@ namespace DanielLochner.Assets
         #endregion
 
         #region Methods
-        private void Start()
+        protected virtual void Start()
         {
             health.OnValueChanged += UpdateHealth;
             health.SetDirty(true);
@@ -65,16 +65,16 @@ namespace DanielLochner.Assets
             OnHealthChanged?.Invoke(Health);
         }
 
-        public void TakeDamage(float damage)
+        public void TakeDamage(float damage, Vector3 force = default)
         {
-            TakeDamageServerRpc(damage);
+            TakeDamageServerRpc(damage, force);
         }
         [ServerRpc(RequireOwnership = false)]
-        private void TakeDamageServerRpc(float damage)
+        private void TakeDamageServerRpc(float damage, Vector3 force)
         {
             if (IsDead) return;
             Health -= damage;
-            TakeDamageClientRpc(damage);
+            TakeDamageClientRpc(damage, force);
 
             if (Health <= 0f)
             {
@@ -82,9 +82,9 @@ namespace DanielLochner.Assets
             }
         }
         [ClientRpc]
-        private void TakeDamageClientRpc(float damage)
+        private void TakeDamageClientRpc(float damage, Vector3 force)
         {
-            OnTakeDamage?.Invoke(damage);
+            OnTakeDamage?.Invoke(damage, force);
         }
 
         public void Die()
