d40c123bfe2c5b14854d0995543945706c2924e3
Daniel Lochner <daniel@daniellochner.com>
2022-11-11 10:20:03 +0200

    Add locked chest quest

---
+++ Assets/LockedChest.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using Unity.Netcode;
+using UnityEngine;
+
+namespace DanielLochner.Assets.CreatureCreator
+{
+    public class LockedChest : NetworkBehaviour
+    {
+        #region Fields
+        [SerializeField] private AudioSource unlockAS;
+        [SerializeField] private Transform capT;
+        private bool isOpen;
+        #endregion
+
+        #region Methods
+        public void TryOpen()
+        {
+            OpenServerRpc();
+        }
+
+        [ServerRpc(RequireOwnership = false)]
+        public void OpenServerRpc()
+        {
+            if (!isOpen)
+            {
+                StartCoroutine(OpenRoutine());
+                isOpen = true;
+            }
+        }
+
+        private IEnumerator OpenRoutine()
+        {
+            OpenClientRpc();
+            yield return new WaitForSeconds(1.25f); // Wait for key unlock sound.
+
+            yield return InvokeUtility.InvokeOverTimeRoutine(delegate (float p)
+            {
+                capT.localRotation = Quaternion.Slerp(Quaternion.identity, Quaternion.Euler(90f, 0f, 0f), p);
+            },
+            1f);
+        }
+
+        [ClientRpc]
+        private void OpenClientRpc()
+        {
+            unlockAS.Play();
+        }
+        #endregion
+    }
+}
