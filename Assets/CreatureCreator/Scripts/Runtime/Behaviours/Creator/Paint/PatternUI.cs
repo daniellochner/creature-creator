41957c4a27eef4c3994e5a8b7c35a65f5d965755
Daniel Lochner <daniel@daniellochner.com>
2022-12-14 11:23:22 +0200

    Add new icon to adventure mode

--- Assets/CreatureCreator/Scripts/Runtime/Behaviours/Creator/Paint/PatternUI.cs
+++ Assets/CreatureCreator/Scripts/Runtime/Behaviours/Creator/Paint/PatternUI.cs
@@ -12,23 +12,35 @@ namespace DanielLochner.Assets.CreatureCreator
         #region Fields
         [SerializeField] private Toggle selectToggle;
         [SerializeField] private ClickUI clickUI;
+        [SerializeField] private HoverUI hoverUI;
         [SerializeField] private CanvasGroup canvasGroup;
+        [SerializeField] private GameObject newGO;
         #endregion
 
         #region Properties
         public Toggle SelectToggle => selectToggle;
         public ClickUI ClickUI => clickUI;
+        public HoverUI HoverUI => hoverUI;
         public CanvasGroup CanvasGroup => canvasGroup;
         #endregion
 
         #region Methods
-        public void Setup(Texture pattern, Material material)
+        public void Setup(Texture pattern, Material material, bool isNew = false)
         {
             Image graphic = selectToggle.graphic as Image;
             Image targetGraphic = selectToggle.targetGraphic as Image;
 
             graphic.sprite = targetGraphic.sprite = Sprite.Create(pattern as Texture2D, new Rect(0, 0, pattern.width, pattern.height), new Vector2(0.5f, 0.5f));
             graphic.material = targetGraphic.material = material;
+
+            if (isNew)
+            {
+                newGO.SetActive(true);
+                hoverUI.OnEnter.AddListener(delegate
+                {
+                    newGO.SetActive(false);
+                });
+            }
         }
         #endregion
     }
