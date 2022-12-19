f3726056d31e9e851469e59f17285eadabf8bf38
Daniel Lochner <daniel@daniellochner.com>
2022-12-19 14:26:31 +0200

    Add IsInteractable to GameObjectUtility

--- Assets/Plugins/Internal/Utilities/GameObjectUtility.cs
+++ Assets/Plugins/Internal/Utilities/GameObjectUtility.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace DanielLochner.Assets
 {
@@ -40,6 +41,43 @@ namespace DanielLochner.Assets
                 return hitInfo.collider.gameObject == gameObject;
             }
             return false;
+        }
+
+        public static bool IsInteractable(this GameObject gameObject)
+        {
+            if (gameObject == null)
+                return false;
+
+            Selectable selectable = gameObject.GetComponent<Selectable>();
+            if (selectable != null && !selectable.interactable)
+                return false;
+
+            List<CanvasGroup> m_CanvasGroupCache = new List<CanvasGroup>();
+            bool interactableCheck = true;
+
+            Transform cg_transform = gameObject.transform;
+            while (cg_transform != null)
+            {
+                cg_transform.GetComponents(m_CanvasGroupCache);
+                bool ignoreParentGroups = false;
+
+                for (int i = 0, count = m_CanvasGroupCache.Count; i < count; i++)
+                {
+                    var canvasGroup = m_CanvasGroupCache[i];
+
+                    interactableCheck &= canvasGroup.interactable;
+                    ignoreParentGroups |= canvasGroup.ignoreParentGroups || !canvasGroup.interactable;
+                }
+
+                if (ignoreParentGroups)
+                {
+                    break;
+                }
+
+                cg_transform = cg_transform.parent;
+            }
+
+            return interactableCheck;
         }
 
         public static T GetOrAddComponent<T>(this GameObject gameObject) where T : Component
