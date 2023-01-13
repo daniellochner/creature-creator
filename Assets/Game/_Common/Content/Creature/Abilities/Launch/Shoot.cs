d65ed140f9a2c16e0676b0f7e5ad1794e7b60a85
Daniel Lochner <daniel@daniellochner.com>
2023-01-13 18:31:10 +0200

    Add spit and shoot abilities

---
+++ Assets/Game/_Common/Content/Creature/Abilities/Launch/Shoot.cs
@@ -0,0 +1,12 @@
+// Creature Creator - https://github.com/daniellochner/Creature-Creator
+// Copyright (c) Daniel Lochner
+
+using UnityEngine;
+
+namespace DanielLochner.Assets.CreatureCreator.Abilities
+{
+    [CreateAssetMenu(menuName = "Creature Creator/Ability/Shoot")]
+    public class Shoot : Launch
+    {
+    }
+}
---
+++ Assets/Game/_Common/Content/Creature/Abilities/Launch/Spit.cs
@@ -0,0 +1,12 @@
+﻿// Creature Creator - https://github.com/daniellochner/Creature-Creator
+// Copyright (c) Daniel Lochner
+
+using UnityEngine;
+
+namespace DanielLochner.Assets.CreatureCreator.Abilities
+{
+    [CreateAssetMenu(menuName = "Creature Creator/Ability/Spit")]
+    public class Spit : Launch
+    {
+    }
+}
