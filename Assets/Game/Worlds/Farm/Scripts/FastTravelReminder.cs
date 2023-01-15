b8449091271f7b52271b86b8bfa88715610c5c35
Daniel Lochner <daniel@daniellochner.com>
2023-01-15 21:48:43 +0200

    Add a reminder to teleport using minimap

--- Assets/Game/Worlds/Farm/Scripts/FastTravelReminder.cs
+++ Assets/Game/Worlds/Farm/Scripts/FastTravelReminder.cs
@@ -3,8 +3,13 @@ using UnityEngine;
 
 namespace DanielLochner.Assets.CreatureCreator
 {
-    public class FastTravelReminder : MonoBehaviour
+    public class FastTravelReminder : MonoBehaviourSingleton<FastTravelReminder>
     {
+        #region Properties
+        public bool Remind { get; set; } = true;
+        #endregion
+
+        #region Methods
         private IEnumerator Start()
         {
             if (PlayerPrefs.GetInt("FAST_TRAVEL_REMINDER") == 0)
@@ -14,7 +19,14 @@ namespace DanielLochner.Assets.CreatureCreator
 
                 InformationDialog.Inform("Fast Travel!", "Click on the minimap to view the entire world. You can pan and zoom using your cursor. Then, click on an editing platform to teleport to it!");
                 PlayerPrefs.SetInt("FAST_TRAVEL_REMINDER", 1);
+
+                yield return new WaitForSeconds(300f);
+                if (Remind)
+                {
+                    InformationDialog.Inform("Fast Travel!", "Remember, you can click on the minimap to view the entire world and then click on an editing platform to teleport to it!");
+                }
             }
         }
+        #endregion
     }
 }
