592a8cb61616d0e72456a434f3c2566df984b6dc
Daniel Lochner <daniel@daniellochner.com>
2023-01-18 02:58:55 +0200

    Create localized fonts database

---
+++ Assets/Plugins/External/Free/Snippets/LocalizeTMProFontEvent.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using System;
+using TMPro;
+using UnityEngine.Localization.Components;
+using UnityEngine.Localization;
+
+/// <summary>
+/// UnityEvent which can pass a Texture as an argument.
+/// </summary>
+[Serializable]
+public class UnityEventFont : UnityEvent<TMP_FontAsset> { }
+
+/// <summary>
+/// Component that can be used to Localize a Texture asset.
+/// Provides an update event that can be used to automatically update the texture whenever the Locale changes.
+/// </summary>
+[AddComponentMenu("Localization/Asset/Localize TMPro Font Event")]
+public class LocalizeTMProFontEvent : LocalizedAssetEvent<TMP_FontAsset, LocalizeTMProFont, UnityEventFont>
+{}
