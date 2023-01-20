9601dcb913bd7e7cc858414dead03227eea6e6df
Daniel Lochner <daniel@daniellochner.com>
2023-01-20 06:43:38 +0200

    Localize dialogs

--- Assets/Plugins/Internal/Keybindings/KeybindingsDialog.cs
+++ Assets/Plugins/Internal/Keybindings/KeybindingsDialog.cs
@@ -29,7 +29,8 @@ namespace DanielLochner.Assets
         #region Methods
         public static void Rebind(KeybindUI keybindUI)
         {
-            Instance.messageText.text = $"Press any key(s) to rebind to \"{keybindUI.Action}\".\nPress ESC to cancel.";
+            // Instance.messageText.text = $"Press any key(s) to rebind to \"{keybindUI.Action}\".\nPress ESC to cancel.";
+            Instance.messageText.SetArguments(keybindUI.Action);
             Instance.StartCoroutine(Instance.RebindRoutine(keybindUI));
         }
         private IEnumerator RebindRoutine(KeybindUI keybindUI)
