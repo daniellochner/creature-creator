afb48ad36171dee21be5bbe1d41ce11e98c03598
Daniel Lochner <daniel@daniellochner.com>
2023-01-29 11:32:14 +0200

    Bake farm lighting

--- Assets/Game/Worlds/Farm/Scripts/FarmTeleporter.cs
+++ Assets/Game/Worlds/Farm/Scripts/FarmTeleporter.cs
@@ -1,14 +1,8 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 namespace DanielLochner.Assets.CreatureCreator
 {
     public class FarmTeleporter : TeleportManager
     {
-        public override void OnEnter(string prevScene, string nextScene, CreatureData data)
-        {
-            base.OnEnter(prevScene, nextScene, data);
-        }
     }
 }
