5ac0ccaaef8fa23f74eb416c7b83363b97123449
Daniel Lochner <daniel@daniellochner.com>
2023-03-17 13:41:44 +0200

    Fix night vision effect

--- Assets/Game/_Common/Content/Creature/Abilities/NightVision/NightVision.cs
+++ Assets/Game/_Common/Content/Creature/Abilities/NightVision/NightVision.cs
@@ -9,19 +9,34 @@ namespace DanielLochner.Assets.CreatureCreator.Abilities
     [CreateAssetMenu(menuName = "Creature Creator/Ability/NightVision")]
     public class NightVision : Ability
     {
-        [Header("NightVision")]
+        [Header("Night Vision")]
+        [SerializeField] private Ability see;
         [SerializeField] private PostProcessProfile defaultProfile;
+        [SerializeField] private PostProcessProfile blindedProfile;
         [SerializeField] private PostProcessProfile nightVisionProfile;
+        [SerializeField] private Light spotlightPrefab;
+
+        private Light spotlight;
 
         public override void OnAdd()
         {
-            PostProcessManager.Instance.BlendToProfile(defaultProfile, 0.25f);
+            PostProcessManager.Instance.BlendToProfile(nightVisionProfile, 0.25f);
             MinimapManager.Instance.SetVisibility(SettingsManager.Data.Map);
+
+            spotlight = Instantiate(spotlightPrefab, Player.Instance.Camera.MainCamera.transform);
+            float target = spotlight.intensity;
+            PostProcessManager.Instance.InvokeOverTime(delegate (float p)
+            {
+                spotlight.intensity = p * target;
+            }, 
+            0.25f);
         }
         public override void OnRemove()
         {
-            PostProcessManager.Instance.BlendToProfile(nightVisionProfile, 0.25f);
+            PostProcessManager.Instance.BlendToProfile(Player.Instance.Abilities.Abilities.Contains(see) ? defaultProfile : blindedProfile, 0.25f);
             MinimapManager.Instance.SetVisibility(false);
+
+            Destroy(spotlight.gameObject);
         }
     }
 }
