2ccff930f5169be341a5eff46b11576ca1450484
Daniel Lochner <daniel@daniellochner.com>
2023-03-17 12:33:18 +0200

    Add night vision ability

---
+++ Assets/Game/_Common/Content/Creature/Abilities/NightVision/NightVision.cs
@@ -0,0 +1,27 @@
+﻿// Creature Creator - https://github.com/daniellochner/Creature-Creator
+// Copyright (c) Daniel Lochner
+
+using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+
+namespace DanielLochner.Assets.CreatureCreator.Abilities
+{
+    [CreateAssetMenu(menuName = "Creature Creator/Ability/NightVision")]
+    public class NightVision : Ability
+    {
+        [Header("NightVision")]
+        [SerializeField] private PostProcessProfile defaultProfile;
+        [SerializeField] private PostProcessProfile nightVisionProfile;
+
+        public override void OnAdd()
+        {
+            PostProcessManager.Instance.BlendToProfile(defaultProfile, 0.25f);
+            MinimapManager.Instance.SetVisibility(SettingsManager.Data.Map);
+        }
+        public override void OnRemove()
+        {
+            PostProcessManager.Instance.BlendToProfile(nightVisionProfile, 0.25f);
+            MinimapManager.Instance.SetVisibility(false);
+        }
+    }
+}
