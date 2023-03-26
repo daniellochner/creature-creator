598a835f35d6eeae2fb655ca1ad2f1e966af2c7c
Daniel Lochner <daniel@daniellochner.com>
2023-03-26 20:08:49 +0200

    Change night vision to toggle to activate

--- Assets/Game/_Common/Content/Creature/Abilities/NightVision/NightVision.cs
+++ Assets/Game/_Common/Content/Creature/Abilities/NightVision/NightVision.cs
@@ -2,6 +2,7 @@
 // Copyright (c) Daniel Lochner
 
 using UnityEngine;
+using UnityEngine.Audio;
 using UnityEngine.Rendering.PostProcessing;
 
 namespace DanielLochner.Assets.CreatureCreator.Abilities
@@ -9,34 +10,55 @@ namespace DanielLochner.Assets.CreatureCreator.Abilities
     [CreateAssetMenu(menuName = "Creature Creator/Ability/NightVision")]
     public class NightVision : Ability
     {
+        #region Fields
         [Header("Night Vision")]
         [SerializeField] private Ability see;
         [SerializeField] private PostProcessProfile defaultProfile;
         [SerializeField] private PostProcessProfile blindedProfile;
         [SerializeField] private PostProcessProfile nightVisionProfile;
+        [SerializeField] private AudioClip enableSound;
+        [SerializeField] private AudioMixerGroup mixer;
         [SerializeField] private Light spotlightPrefab;
 
         private Light spotlight;
+        private bool isEnabled;
+        #endregion
 
-        public override void OnAdd()
+        #region Methods
+        public override void OnPerform()
         {
-            PostProcessManager.Instance.BlendToProfile(nightVisionProfile, 0.25f);
-            MinimapManager.Instance.SetVisibility(SettingsManager.Data.Map);
-
-            spotlight = Instantiate(spotlightPrefab, Player.Instance.Camera.MainCamera.transform);
-            float target = spotlight.intensity;
-            PostProcessManager.Instance.InvokeOverTime(delegate (float p)
-            {
-                spotlight.intensity = p * target;
-            }, 
-            0.25f);
+            SetNightVision(isEnabled = !isEnabled);
         }
         public override void OnRemove()
         {
-            PostProcessManager.Instance.BlendToProfile(Player.Instance.Abilities.Abilities.Contains(see) ? defaultProfile : blindedProfile, 0.25f);
-            MinimapManager.Instance.SetVisibility(false);
-
-            Destroy(spotlight.gameObject);
+            if (isEnabled)
+            {
+                SetNightVision(isEnabled = false);
+            }
         }
+
+        private void SetNightVision(bool isEnabled)
+        {
+            if (isEnabled)
+            {
+                PostProcessManager.Instance.BlendToProfile(nightVisionProfile, 0.25f);
+
+                spotlight = Instantiate(spotlightPrefab, Player.Instance.Camera.MainCamera.transform);
+                float target = spotlight.intensity;
+                PostProcessManager.Instance.InvokeOverTime(delegate (float p)
+                {
+                    spotlight.intensity = p * target;
+                },
+                0.25f);
+
+                AudioSourceUtility.PlayClipAtPoint(enableSound, null, 1, mixer);
+            }
+            else
+            {
+                PostProcessManager.Instance.BlendToProfile(Player.Instance.Abilities.Abilities.Contains(see) ? defaultProfile : blindedProfile, 0.25f);
+                Destroy(spotlight.gameObject);
+            }
+        }
+        #endregion
     }
 }
