19f640db5675fe30e3b38e590f252344ae068136
Daniel Lochner <daniel@daniellochner.com>
2023-03-27 19:56:28 +0200

    Fix holdable dummy position

---
+++ Assets/CreatureCreator/Scripts/Runtime/Behaviours/Creature/BodyParts/Constructors/Extremities/HandConstructor.cs
@@ -0,0 +1,23 @@
+﻿// Creature Creator - https://github.com/daniellochner/Creature-Creator
+// Copyright (c) Daniel Lochner
+
+using UnityEngine;
+
+namespace DanielLochner.Assets.CreatureCreator
+{
+    public class HandConstructor : ExtremityConstructor
+    {
+        #region Fields
+        [Header("Hand")]
+        [SerializeField] private Transform palm;
+        #endregion
+
+        #region Properties
+        public ArmConstructor ConnectedArm => ConnectedLimb as ArmConstructor;
+
+        public HandConstructor FlippedHand => Flipped as HandConstructor;
+
+        public Transform Palm => palm;
+        #endregion
+    }
+}
