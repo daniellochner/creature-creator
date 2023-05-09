0cd3a475c063bb0a06eb25e3e3dbc60cd03a9b34
Daniel Lochner <daniel@daniellochner.com>
2023-05-09 20:47:55 +0200

    Set all non-premium parts by default

--- Assets/Game/Other/Testing/Testing.cs
+++ Assets/Game/Other/Testing/Testing.cs
@@ -1,20 +1,4 @@
-using DanielLochner.Assets;
-using DanielLochner.Assets.CreatureCreator;
 using UnityEngine;
 public class Testing : MonoBehaviour
 {
-    [ContextMenu("SetALl")]
-    public void SetAll()
-    {
-        foreach (UnlockableBodyPart ubp in FindObjectsOfType<UnlockableBodyPart>(true))
-        {
-            DatabaseManager.GetDatabaseEntry<BodyPart>("Body Parts", ubp.BodyPartID).premium = false;
-
-        }
-        foreach (UnlockablePattern p in FindObjectsOfType<UnlockablePattern>(true))
-        {
-            DatabaseManager.GetDatabaseEntry<Pattern>("Patterns", p.PatternID).premium = false;
-
-        }
-    }
 }
