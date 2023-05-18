f9f6a1f538f70a1e7c6447a910aae727b6553472
Daniel Lochner <daniel@daniellochner.com>
2023-05-18 10:49:44 +0200

    Add SafeArea plugin

---
+++ Assets/Plugins/External/Free/CrystalFramework/Utility/Demos/SafeAreaDemo.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System;
+
+namespace Crystal
+{
+    public class SafeAreaDemo : MonoBehaviour
+    {
+        [SerializeField] KeyCode KeySafeArea = KeyCode.A;
+        SafeArea.SimDevice[] Sims;
+        int SimIdx;
+
+        void Awake ()
+        {
+            if (!Application.isEditor)
+                Destroy (this);
+
+            Sims = (SafeArea.SimDevice[])Enum.GetValues (typeof (SafeArea.SimDevice));
+        }
+
+        void Update ()
+        {
+            if (Input.GetKeyDown (KeySafeArea))
+                ToggleSafeArea ();
+        }
+
+        /// <summary>
+        /// Toggle the safe area simulation device.
+        /// </summary>
+        void ToggleSafeArea ()
+        {
+            SimIdx++;
+
+            if (SimIdx >= Sims.Length)
+                SimIdx = 0;
+
+            SafeArea.Sim = Sims[SimIdx];
+            Debug.LogFormat ("Switched to sim device {0} with debug key '{1}'", Sims[SimIdx], KeySafeArea);
+        }
+    }
+}
