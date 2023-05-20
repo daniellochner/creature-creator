82fad22bd0980c01c6a1f4753d73529d3510c47c
Daniel Lochner <daniel@daniellochner.com>
2023-05-20 21:29:28 +0200

    Add PostProcessPlistIOS for NSUserTrackingUsageDescription

---
+++ Assets/Game/_Common/Scripts/Editor/PostProcessPlistIOS.cs
@@ -0,0 +1,31 @@
+using System.IO;
+using UnityEditor;
+using UnityEditor.Callbacks;
+using UnityEditor.iOS.Xcode;
+
+namespace DanielLochner.Assets.CreatureCreator
+{
+    public class PostProcessPlistIOS
+    {
+        [PostProcessBuild]
+        public static void ChangeXcodePlist(BuildTarget buildTarget, string pathToBuiltProject)
+        {
+            if (buildTarget == BuildTarget.iOS)
+            {
+                // Read plist
+                string plistPath = pathToBuiltProject + "/Info.plist";
+                PlistDocument plist = new PlistDocument();
+                plist.ReadFromString(File.ReadAllText(plistPath));
+
+                // Get plist's root
+                PlistElementDict rootDict = plist.root;
+
+                // Set "Privacy - Tracking Usage Description" string
+                rootDict.SetString("NSUserTrackingUsageDescription", "This data will ONLY be used to deliver personalized ads.");
+
+                // Write plist
+                File.WriteAllText(plistPath, plist.WriteToString());
+            }
+        }
+    }
+}
