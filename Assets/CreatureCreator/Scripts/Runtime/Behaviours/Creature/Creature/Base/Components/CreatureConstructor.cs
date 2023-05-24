db545998b61c719ea11f7df70dc740675b257b4e
Daniel Lochner <daniel@daniellochner.com>
2023-05-24 14:38:37 +0200

    Add function to update bounds
    Necessary because skinned mesh bounds incorrect due to blend shapes

--- Assets/CreatureCreator/Scripts/Runtime/Behaviours/Creature/Creature/Base/Components/CreatureConstructor.cs
+++ Assets/CreatureCreator/Scripts/Runtime/Behaviours/Creature/Creature/Base/Components/CreatureConstructor.cs
@@ -155,7 +155,6 @@ namespace DanielLochner.Assets.CreatureCreator
             SetOffset(data.Offset);
             SetShine(data.Shine);
             SetMetallic(data.Metallic);
-            UpdateDimensions();
 
             if (debug) Timer.Stop("Creature");
         }
@@ -799,6 +798,49 @@ namespace DanielLochner.Assets.CreatureCreator
             statistics.HealthBody = (int)Mathf.Lerp(minMaxBodyHealth.min, minMaxBodyHealth.max, w);
 
             rb.mass = statistics.Weight;
+        }
+        public void UpdateBounds()
+        {
+            Mesh tmpMesh = new Mesh();
+            SkinnedMeshRenderer.BakeMesh(tmpMesh);
+
+            MinMax minMaxX = new MinMax(Mathf.Infinity, Mathf.NegativeInfinity);
+            MinMax minMaxY = new MinMax(Mathf.Infinity, Mathf.NegativeInfinity);
+            MinMax minMaxZ = new MinMax(Mathf.Infinity, Mathf.NegativeInfinity);
+            foreach (Vector3 vertex in tmpMesh.vertices)
+            {
+                if (vertex.x < minMaxX.min)
+                {
+                    minMaxX.min = vertex.x;
+                }
+                if (vertex.x > minMaxX.max)
+                {
+                    minMaxX.max = vertex.x;
+                }
+
+                if (vertex.y < minMaxY.min)
+                {
+                    minMaxY.min = vertex.y;
+                }
+                if (vertex.y > minMaxY.max)
+                {
+                    minMaxY.max = vertex.y;
+                }
+
+                if (vertex.z < minMaxZ.min)
+                {
+                    minMaxZ.min = vertex.z;
+                }
+                if (vertex.z > minMaxZ.max)
+                {
+                    minMaxZ.max = vertex.z;
+                }
+            }
+            Vector3 center = new Vector3(minMaxX.Average, minMaxY.Average, minMaxZ.Average);
+            Vector3 size = new Vector3(minMaxX.Range, minMaxY.Range, minMaxZ.Range);
+            SkinnedMeshRenderer.localBounds = new UnityEngine.Bounds(center, size);
+
+            Destroy(tmpMesh);
         }
         #endregion
     }
