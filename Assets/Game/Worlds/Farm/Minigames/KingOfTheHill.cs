fd92f984fc733aaf95eef3be378396ace6965061
Daniel Lochner <daniel@daniellochner.com>
2023-06-12 23:03:14 +0200

    Add base minigames

---
+++ Assets/Game/Worlds/Farm/Minigames/KingOfTheHill.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DanielLochner.Assets.CreatureCreator
+{
+    public class KingOfTheHill : Minigame
+    {
+        public override Transform GetSpawnPoint()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override void OnSetupScoreboard()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        protected override List<ulong> GetWinnerClientIds()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        protected override string GetWinnerName()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
