ef36bdb7b0bdf9b9453b3b9ce936a386128dcaa7
Daniel Lochner <daniel@daniellochner.com>
2023-06-19 18:24:02 +0200

    Add target

---
+++ Assets/Plugins/Internal/Target/Target.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class Target : MonoBehaviour
+{
+    #region Fields
+    [SerializeField] private MeshRenderer meshRenderer;
+    [SerializeField] private Transform fill;
+    [SerializeField] private float speed;
+    [SerializeField] private UnityEvent onEnter;
+
+    private bool hasEntered, hasCompleted;
+    private float progress;
+    private Material material;
+    #endregion
+
+    #region Properties
+    public bool IsHeld { get; set; }
+    #endregion
+
+    #region Methods
+    private void Awake()
+    {
+        material = new Material(meshRenderer.sharedMaterial);
+        meshRenderer.sharedMaterial = material;
+    }
+    public void Update()
+    {
+        material.SetFloat("_MinTransparency", 0.25f + Mathf.PingPong(Time.time, 0.75f));
+        progress = Mathf.Clamp01(progress + ((hasEntered || IsHeld) ? speed : -speed) * Time.deltaTime);
+
+        fill.localScale = Vector3.Lerp(new Vector3(0f, 1f, 0f), Vector3.one, progress);
+        if (progress >= 1 && !hasCompleted)
+        {
+            onEnter.Invoke();
+            hasCompleted = true;
+        }
+        else
+        if (progress <= 0)
+        {
+            hasCompleted = false;
+        }
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player/Local"))
+        {
+            hasEntered = true;
+        }
+    }
+    public void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player/Local"))
+        {
+            hasEntered = false;
+        }
+    }
+    #endregion
+}
