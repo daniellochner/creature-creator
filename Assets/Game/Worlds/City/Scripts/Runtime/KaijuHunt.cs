1c1a48e3bcec783a50f83c413f52d3227c865fc2
Daniel Lochner <daniel@daniellochner.com>
2023-06-25 14:19:37 +0200

    Add base city CC components

---
+++ Assets/Game/Worlds/City/Scripts/Runtime/KaijuHunt.cs
@@ -0,0 +1,28 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DanielLochner.Assets.CreatureCreator
+{
+    public class KaijuHunt : Minigame
+    {
+        public override Transform GetSpawnPoint()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override void OnSetupScoreboard()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        protected override List<ulong> GetWinnerClientIds()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        protected override string GetWinnerName()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
