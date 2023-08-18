08d5a85efffc4f73aff137e1b79d2990bc9817a3
Daniel Lochner <daniel@daniellochner.com>
2023-08-18 10:56:25 +0200

    Fix CCSteamManager

--- Assets/Game/_Common/Scripts/Runtime/Behaviours/CCSteamManager.cs
+++ Assets/Game/_Common/Scripts/Runtime/Behaviours/CCSteamManager.cs
@@ -2,11 +2,12 @@ using UnityEngine;
 
 namespace DanielLochner.Assets.CreatureCreator
 {
+    [DefaultExecutionOrder(1)]
     public class CCSteamManager : SteamManager
     {
         protected override void Awake()
         {
-            if (Application.version.EndsWith("-edu"))
+            if (EducationManager.Instance.IsEducational)
             {
                 enabled = false;
             }
