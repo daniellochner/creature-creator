8256d1811f59846e8c4e77428c477a3282ea9bd8
Daniel Lochner <daniel@daniellochner.com>
2023-08-25 16:39:03 +0200

    Add shop button to main menu

---
+++ Assets/Game/MainMenu/Scripts/Runtime/Behaviours/Shop/ShopMenu.cs
@@ -0,0 +1,32 @@
+// Creature Creator - https://github.com/daniellochner/Creature-Creator
+// Copyright (c) Daniel Lochner
+
+using UnityEngine;
+
+namespace DanielLochner.Assets.CreatureCreator
+{
+    public class ShopMenu : MenuSingleton<ShopMenu>
+    {
+        #region Fields
+        [SerializeField] private GameObject shopButton;
+        #endregion
+
+        #region Methods
+        private void Start()
+        {
+            shopButton.SetActive(!SettingsManager.Instance.ShowTutorial);
+        }
+
+        public override void OnEndOpen()
+        {
+            base.OnEndOpen();
+            Visit();
+        }
+
+        public void Visit()
+        {
+            Application.OpenURL("https://playcreature.com/merch");
+        }
+        #endregion
+    }
+}
