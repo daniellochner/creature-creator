504ac89cb68a5237a9b745f385a19adce34d56ac
Daniel Lochner <daniel@daniellochner.com>
2023-09-06 16:44:39 +0200

    Fix fast travel reminder

--- Assets/Game/Worlds/Farm/Scripts/FastTravelReminder.cs
+++ Assets/Game/Worlds/Farm/Scripts/FastTravelReminder.cs
@@ -6,33 +6,34 @@ namespace DanielLochner.Assets.CreatureCreator
     public class FastTravelReminder : MonoBehaviourSingleton<FastTravelReminder>
     {
         #region Properties
+        public bool HasReminded
+        {
+            get => PlayerPrefs.GetInt("FAST_TRAVEL_REMINDER", 0) == 1;
+            set => PlayerPrefs.SetInt("FAST_TRAVEL_REMINDER", value ? 1 : 0);
+        }
+
         public bool Remind { get; set; } = true;
         #endregion
 
         #region Methods
         private IEnumerator Start()
         {
-            if (SettingsManager.Data.Map && PlayerPrefs.GetInt("FAST_TRAVEL_REMINDER") == 0)
+            if (SettingsManager.Data.Map && !HasReminded)
             {
                 yield return new WaitUntil(() => EditorManager.Instance.IsPlaying);
                 yield return new WaitForSeconds(1f);
 
-                Show();
-                PlayerPrefs.SetInt("FAST_TRAVEL_REMINDER", 1);
+                HasReminded = true;
 
-                yield return new WaitForSeconds(300f);
-                if (Remind)
+                while (Remind)
                 {
-                    Show();
+                    string title = LocalizationUtility.Localize("cc_fast-travel_title");
+                    string message = LocalizationUtility.Localize($"cc_fast-travel_message_{SystemUtility.DeviceType}".ToLower());
+                    InformationDialog.Inform(title, message);
+
+                    yield return new WaitForSeconds(300f);
                 }
             }
-        }
-
-        private void Show()
-        {
-            string title = LocalizationUtility.Localize("cc_fast-travel_title");
-            string message = LocalizationUtility.Localize($"cc_fast-travel_message_{SystemUtility.DeviceType}".ToLower());
-            InformationDialog.Inform(title, message);
         }
         #endregion
     }
