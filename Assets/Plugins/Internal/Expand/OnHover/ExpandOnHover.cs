21987f9006277762fcbad30ee9d635e35cd324d7
Daniel Lochner <daniel@daniellochner.com>
2024-05-27 22:18:50 +0200

    Remove Animator from all BodyPart and Pattern UI

--- Assets/Plugins/Internal/Expand/OnHover/ExpandOnHover.cs
+++ Assets/Plugins/Internal/Expand/OnHover/ExpandOnHover.cs
@@ -1,14 +1,16 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.Events;
 
 namespace DanielLochner.Assets
 {
-    [RequireComponent(typeof(Animator), typeof(HoverUI))]
+    [RequireComponent(typeof(HoverUI))]
     public class ExpandOnHover : MonoBehaviour
     {
         #region Fields
-        [SerializeField] private Animator animator;
         [SerializeField] private HoverUI hoverUI;
+
+        private Coroutine expandCoroutine;
         #endregion
 
         #region Methods
@@ -18,13 +20,49 @@ namespace DanielLochner.Assets
             {
                 if (!Input.GetMouseButton(0))
                 {
-                    animator.SetBool("Expanded", true);
+                    SetExpanded(true);
                 }
             });
             hoverUI.OnExit.AddListener(delegate
             {
-                animator.SetBool("Expanded", false);
+                SetExpanded(false);
             });
+        }
+        private void OnDisable()
+        {
+            SetScale(1f);
+        }
+
+        public void SetExpanded(bool isExpanded)
+        {
+            this.StopStartCoroutine(ExpandRoutine(isExpanded), ref expandCoroutine);
+        }
+
+        private IEnumerator ExpandRoutine(bool isExpanded)
+        {
+            float initialScale = transform.localScale.x;
+            if (isExpanded)
+            {
+                for (float i = initialScale; i < 1.075f; i += (Time.unscaledDeltaTime / 0.25f) * 0.075f)
+                {
+                    SetScale(i);
+                    yield return null;
+                }
+                SetScale(1.075f);
+            }
+            else
+            {
+                for (float i = initialScale; i > 1f; i -= (Time.unscaledDeltaTime / 0.25f) * 0.075f)
+                {
+                    SetScale(i);
+                    yield return null;
+                }
+                SetScale(1f);
+            }
+        }
+        private void SetScale(float scale)
+        {
+            transform.localScale = Vector3.one * scale;
         }
         #endregion
     }
