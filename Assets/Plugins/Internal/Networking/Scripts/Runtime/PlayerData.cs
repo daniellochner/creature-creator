5a77ed9f2abd7e8ca4ff13a719dfd385be584db9
Daniel Lochner <daniel@daniellochner.com>
2024-08-19 11:48:12 +0200

    Update project version and packages

--- Assets/Plugins/Internal/Networking/Scripts/Runtime/PlayerData.cs
+++ Assets/Plugins/Internal/Networking/Scripts/Runtime/PlayerData.cs
@@ -4,12 +4,17 @@ using System;
 namespace DanielLochner.Assets
 {
     [Serializable]
-    public class PlayerData : INetworkSerializable
+    public class PlayerData : INetworkSerializable, IEquatable<PlayerData>
     {
         public ulong clientId;
         public string playerId;
         public string username;
         public int level;
+
+        public bool Equals(PlayerData other)
+        {
+            return clientId.Equals(other.clientId) && playerId.Equals(other.playerId);
+        }
 
         public void NetworkSerialize<T>(BufferSerializer<T> serializer) where T : IReaderWriter
         {
