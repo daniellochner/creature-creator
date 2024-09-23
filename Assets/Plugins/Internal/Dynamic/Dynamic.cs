078aa3482a4029e512e244e37622b2c815a0a7eb
Daniel Lochner <daniel@daniellochner.com>
2024-09-23 21:29:05 +0200

    Fix chat menu not visible on mobile devices with notches

--- Assets/Plugins/Internal/Dynamic/Dynamic.cs
+++ Assets/Plugins/Internal/Dynamic/Dynamic.cs
@@ -8,12 +8,14 @@ namespace DanielLochner.Assets
         [SerializeField] private new Transform transform;
         [SerializeField] private Transform worldCanvas;
         [SerializeField] private Transform overlayCanvas;
+        [SerializeField] private Transform overlayCanvasSafe;
         #endregion
 
         #region Properties
         public static Transform Transform { get { return Instance.transform; } }
         public static Transform WorldCanvas { get { return Instance.worldCanvas; } }
         public static Transform OverlayCanvas { get { return Instance.overlayCanvas; } }
+        public static Transform OverlayCanvasSafe { get { return Instance.overlayCanvasSafe; } }
         #endregion
     }
 }
