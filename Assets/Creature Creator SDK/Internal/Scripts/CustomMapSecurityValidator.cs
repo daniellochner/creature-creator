3096b23ee6bdf2c6f140dd9e51ebc2a6f10f1c3f
Rug <rugbugredfern@gmail.com>
2024-10-19 14:55:15 -0700

    Finished local custom map support

--- Assets/Creature Creator SDK/Internal/Scripts/CustomMapSecurityValidator.cs
+++ Assets/Creature Creator SDK/Internal/Scripts/CustomMapSecurityValidator.cs
@@ -42,6 +42,7 @@ public static class CustomMapSecurityValidator
 		typeof(TMP_Text),
 		typeof(CameraFacingBillboard),
 		typeof(Terrain),
+		typeof(PlatformProxy),
 	};
 
 	public static bool IsGameObjectValid(GameObject go, out string error)
